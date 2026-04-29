d5aa183f12ff162e98468cf3756a28d2962d9227
Jiahao Ye <jiahaoye0427@gmail.com>
2026-04-29 21:35:50 +0200

    terraform: migrar a modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,67 @@ terraform {
 }
 
 provider "proxmox" {
-  endpoint = "https://192.168.100.1:8006/api2/json"
+  endpoint  = "https://192.168.100.1:8006/api2/json"
   api_token = var.proxmox_api_token
   insecure  = true
+}
+
+module "wireguard" {
+  source         = "./modules/vm"
+  vm_id          = 101
+  name           = "wireguard-vpn"
+  cores          = 1
+  memory         = 2048
+  proxmox_node   = var.proxmox_node
+  template_vm_id = var.template_vm_id
+  network_bridge = var.network_bridge
+  cpu_type       = var.cpu_type
+}
+
+module "nginx" {
+  source         = "./modules/vm"
+  vm_id          = 102
+  name           = "nginx-proxy"
+  cores          = 1
+  memory         = 2048
+  proxmox_node   = var.proxmox_node
+  template_vm_id = var.template_vm_id
+  network_bridge = var.network_bridge
+  cpu_type       = var.cpu_type
+}
+
+module "nextcloud" {
+  source         = "./modules/vm"
+  vm_id          = 103
+  name           = "nextcloud"
+  cores          = 2
+  memory         = 4096
+  proxmox_node   = var.proxmox_node
+  template_vm_id = var.template_vm_id
+  network_bridge = var.network_bridge
+  cpu_type       = var.cpu_type
+}
+
+module "vaultwarden" {
+  source         = "./modules/vm"
+  vm_id          = 104
+  name           = "vaultwarden"
+  cores          = 1
+  memory         = 2048
+  proxmox_node   = var.proxmox_node
+  template_vm_id = var.template_vm_id
+  network_bridge = var.network_bridge
+  cpu_type       = var.cpu_type
+}
+
+module "ansible" {
+  source         = "./modules/vm"
+  vm_id          = 120
+  name           = "ansible-control"
+  cores          = 2
+  memory         = 4096
+  proxmox_node   = var.proxmox_node
+  template_vm_id = var.template_vm_id
+  network_bridge = var.network_bridge
+  cpu_type       = var.cpu_type
 }
---
+++ terraform/modules/vm/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.73"
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "vm" {
+  node_name = var.proxmox_node
+  vm_id     = var.vm_id
+  name      = var.name
+
+  clone {
+    vm_id = var.template_vm_id
+    full  = true
+  }
+
+  agent {
+    enabled = false
+  }
+
+  cpu {
+    cores = var.cores
+    type  = var.cpu_type
+  }
+
+  memory {
+    dedicated = var.memory
+  }
+
+  network_device {
+    bridge = var.network_bridge
+  }
+
+  started = true
+}
